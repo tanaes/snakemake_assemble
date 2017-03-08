c65938b2a944777741fb70d6037e5198e9f89cc6
Jon Sanders <jsanders@oeb.harvard.edu>
2017-03-08 03:19:12 -0500

    improved cluster logging

--- launch.sh
+++ launch.sh
@@ -5,4 +5,9 @@ if [ "$#" -lt 1 ]; then
     exit
 fi
 
-snakemake -j 16 --local-cores 4 -w 90 --cluster-config cluster.json --cluster "qsub -k eo -m n -l nodes=1:ppn={cluster.n} -l mem={cluster.mem}gb -l walltime={cluster.time}" --directory "$@"
+outdir=$@
+
+mkdir $outdir/cluster_logs
+
+snakemake -j 16 --local-cores 4 -w 90 --cluster-config cluster.json --cluster "qsub -e $outdir/cluster_logs/{rule}_{wildcards.sample}.err -o $outdir/cluster_logs/{rule}_{wildcards.sample}.out -m n -l nodes=1:ppn={cluster.n} -l mem={cluster.mem}gb -l walltime={cluster.time}" --directory "$@"
+
