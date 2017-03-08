6ceb1f4d3f6ebfc9123e9d9efa851dda5a8d6977
Jon Sanders <jsanders@oeb.harvard.edu>
2017-03-08 16:52:57 -0500

    fixing launch mkdir

--- launch.sh
+++ launch.sh
@@ -7,7 +7,7 @@ fi
 
 outdir=$1
 
-mkdir $outdir/cluster_logs
+mkdir -p $outdir/cluster_logs
 
 snakemake -j 16 --local-cores 4 -w 90 --cluster-config cluster.json --cluster "qsub -e $outdir/cluster_logs/{rule}_{wildcards.sample}.err -o $outdir/cluster_logs/{rule}_{wildcards.sample}.out -m n -l nodes=1:ppn={cluster.n} -l mem={cluster.mem}gb -l walltime={cluster.time}" --directory "$@"
 
