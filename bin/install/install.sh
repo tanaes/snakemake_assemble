3b261f33fecf08e95018220237d51168e3c92e78
Jon Sanders <jsanders@oeb.harvard.edu>
2017-03-07 23:14:16 -0500

    Bug fixes

--- bin/install/install.sh
+++ bin/install/install.sh
@@ -1,11 +1,11 @@
 # create conda env
 conda env create -f bin/install/requirements.yaml
 
-source activate conda
+source activate snakemake_assemble
 
 # download and install quast
 
 wget https://downloads.sourceforge.net/project/quast/quast-4.4.tar.gz
 tar -xzf quast-4.4.tar.gz
 cd quast-4.4
-pip install -e .
+pip install .
