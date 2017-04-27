290250fc4c68a333c923d52f32c392ad2bdd5f47
Sergey Nurk <sergeynurk@gmail.com>
2017-04-27 08:19:02 -0700

    Revert "phylophlan running module added"
    This reverts commit a867af7b0bdb72bdcc972e19f26185908a4cc58c.

--- launch_local.sh
+++ launch_local.sh
@@ -9,8 +9,5 @@ outdir=$1
 
 mkdir -p $outdir/cluster_logs
 
-#source activate snakemake_assemble 
-export PATH=$PATH:$(pwd)/bin:$(pwd)/bin/scripts
+snakemake -j 16 --local-cores 4 -w 90 --cluster-config cluster.json --cluster "touch {cluster.output}; " --directory "$@"
 
-snakemake -j 16 --local-cores 4 -w 90 --cluster-config cluster.json --cluster "touch {cluster.output}; " --directory "$@" all phylophlan_all
-#source deactivate 
