7180e837539ef4eacea5493782453f7cc95a2629
Sergey Nurk <sergeynurk@gmail.com>
2017-04-27 08:18:51 -0700

    Revert "main environment is experimentally activated via bash master scripts"
    This reverts commit 8fb5984cd308e80cfa00b96162d9dbc74ae158f9.

--- Snakefile
+++ Snakefile
@@ -4,8 +4,8 @@ import tempfile
 configfile: "config.yaml"
 localrules: phylophlan_prep, phylophlan_post, phylophlan_prep_combined, phylophlan_post_combined
 
-#ENV = config["env"]
-#shell.prefix("set +u; " + ENV + "; set -u; ")
+ENV = config["env"]
+shell.prefix("set +u; " + ENV + "; set -u; ")
 
 TMP_DIR_ROOT = config['tmp_dir_root']
 
--- launch.sh
+++ launch.sh
@@ -9,9 +9,9 @@ outdir=$1
 
 mkdir -p $outdir/cluster_logs
 
-source activate snakemake_assemble 
+#source activate snakemake_assemble 
 export PATH=$PATH:$(pwd)/bin:$(pwd)/bin/scripts
 
 snakemake -j 16 --local-cores 4 -w 90 --cluster-config cluster.json --cluster "qsub -V -e {cluster.error} -o {cluster.output} -m n -l nodes=1:ppn={cluster.n} -l mem={cluster.mem}gb -l walltime={cluster.time}" --directory "$@" all phylophlan_all
 
-source deactivate
+#source deactivate
