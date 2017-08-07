5944d0d89014927d171d86c6944a7e15c5ebd346
Tanaes <jonsan@gmail.com>
2017-08-07 08:22:55 -0700

    added dag pdfs

---
+++ make_dags.sh
@@ -0,0 +1,5 @@
+for rule in all assemble bin function taxonomy raw qc 
+do
+    bash launch.sh ./ ${rule} --dag | dot -Tsvg > dags/dag_${rule}.svg
+    cairosvg dags/dag_${rule}.svg -o dags/dag_${rule}.pdf
+done
