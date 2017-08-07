d0ea0e52acefaac475b613631459fc338b9d9925
Tanaes <jonsan@gmail.com>
2017-08-07 08:46:46 -0700

    added anvio rule to dags

--- make_dags.sh
+++ make_dags.sh
@@ -1,4 +1,4 @@
-for rule in all assemble bin function taxonomy raw qc 
+for rule in all anvio assemble bin function taxonomy raw qc 
 do
     bash launch.sh ./ ${rule} --dag | dot -Tsvg > dags/dag_${rule}.svg
     cairosvg dags/dag_${rule}.svg -o dags/dag_${rule}.pdf
